3c3a992c14ff2e391b42aeabae88a722c5d89de3
vaniashows <vania6600@gmail.com>
2018-10-13 11:31:25 +0300

    init

---
+++ core/src/main/java/by/itechart/App.java
@@ -0,0 +1,13 @@
+package by.itechart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
