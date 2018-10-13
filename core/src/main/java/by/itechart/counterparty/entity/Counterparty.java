17ae9c5528263c945e8dd73255196c326cc4b077
chep-by <chep_by@mail.ru>
2018-10-13 13:17:24 +0300

    Task #779244 Создать сущность "Контрагент"

---
+++ core/src/main/java/by/itechart/counterparty/entity/Counterparty.java
@@ -0,0 +1,60 @@
+package by.itechart.counterparty.entity;
+
+import by.itechart.common.entity.Address;
+import by.itechart.common.entity.BaseEntity;
+import by.itechart.company.entity.Company;
+import by.itechart.counterparty.enums.CounterpartyType;
+
+import javax.persistence.Embedded;
+import javax.persistence.ManyToOne;
+
+public class Counterparty extends BaseEntity {
+
+    @ManyToOne
+    private Company company;
+    private CounterpartyType counterpartyType;
+    private String name;
+    private String taxNumber;
+    @Embedded
+    private Address address;
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+
+    public CounterpartyType getCounterpartyType() {
+        return counterpartyType;
+    }
+
+    public void setCounterpartyType(CounterpartyType counterpartyType) {
+        this.counterpartyType = counterpartyType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTaxNumber() {
+        return taxNumber;
+    }
+
+    public void setTaxNumber(String taxNumber) {
+        this.taxNumber = taxNumber;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+}
