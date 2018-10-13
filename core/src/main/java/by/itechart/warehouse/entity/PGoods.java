9503855262ca93aaa741160d0fccccc0071a16f1
chep-by <chep_by@mail.ru>
2018-10-13 13:41:59 +0300

    Небольшие правки Task #779247 Создать сущность "Товар помещения"

---
+++ core/src/main/java/by/itechart/warehouse/entity/PGoods.java
@@ -0,0 +1,34 @@
+package by.itechart.warehouse.entity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class PGoods {
+    private Goods goods;
+    private Integer count;
+    private Integer storageTimeDays;
+
+    public Goods getGoods() {
+        return goods;
+    }
+
+    public void setGoods(Goods goods) {
+        this.goods = goods;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    public Integer getStorageTimeDays() {
+        return storageTimeDays;
+    }
+
+    public void setStorageTimeDays(Integer storageTimeDays) {
+        this.storageTimeDays = storageTimeDays;
+    }
+}
--- core/src/main/java/by/itechart/warehouse/entity/Placement.java
+++ core/src/main/java/by/itechart/warehouse/entity/Placement.java
@@ -15,4 +15,44 @@ public class Placement {
     private Integer costOfStorage;
     @OneToMany
     private List<PGoods> pGoodsList;
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+
+    public PlacementType getPlacementType() {
+        return placementType;
+    }
+
+    public void setPlacementType(PlacementType placementType) {
+        this.placementType = placementType;
+    }
+
+    public UnitOfMeasurement getUnitOfMeasurement() {
+        return unitOfMeasurement;
+    }
+
+    public void setUnitOfMeasurement(UnitOfMeasurement unitOfMeasurement) {
+        this.unitOfMeasurement = unitOfMeasurement;
+    }
+
+    public Integer getCostOfStorage() {
+        return costOfStorage;
+    }
+
+    public void setCostOfStorage(Integer costOfStorage) {
+        this.costOfStorage = costOfStorage;
+    }
+
+    public List<PGoods> getpGoodsList() {
+        return pGoodsList;
+    }
+
+    public void setpGoodsList(List<PGoods> pGoodsList) {
+        this.pGoodsList = pGoodsList;
+    }
 }
