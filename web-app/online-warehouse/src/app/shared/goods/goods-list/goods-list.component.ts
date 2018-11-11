16930d2b61671546466137e577907f7d660f57a9
chep-by <chep_by@mail.ru>
2018-11-11 11:27:37 +0300

    Task #785560 migrating to Angular Material: good-list, create-write-off-act

--- web-app/online-warehouse/src/app/shared/goods/goods-list/goods-list.component.ts
+++ web-app/online-warehouse/src/app/shared/goods/goods-list/goods-list.component.ts
@@ -1,9 +1,11 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {GoodService} from "../good.service";
 import {GoodsDto} from "../goods.dto";
 import {PageEvent} from "@angular/material";
 import {Page} from "../../pagination/page";
 import {Pageable} from "../../pagination/pageable";
+import {BehaviorSubject, of} from "rxjs";
+import {catchError, finalize} from "rxjs/operators";
 
 @Component({
   selector: 'app-goods-list',
@@ -13,33 +15,51 @@ import {Pageable} from "../../pagination/pageable";
 export class GoodsListComponent implements OnInit {
 
   @Output() goodsSelected: EventEmitter<GoodsDto> = new EventEmitter();
-  page: Page<GoodsDto>;
-  pageable: Pageable = new Pageable(0, 2);
-  pageSizeOptions: number[] = [2, 3, 5];
+
+  @Input() addButton: Boolean;
+
+  private displayedColumns = ["name", "placementType", "measurementUnit", "cost", "weight", "labelling", "description"];
+  private loadingSubject = new BehaviorSubject<boolean>(false);
+  loading$ = this.loadingSubject.asObservable();
+  private page: Page<GoodsDto>;
+
+
+  private pageable: Pageable = new Pageable(0, 10);
+  private pageSizeOptions: number[] = [10, 25, 50];
+
+  private errors: any[];
 
   constructor(private goodsService: GoodService) {
   }
 
   ngOnInit() {
-    this.getGoods();
+    this.loadGoods();
   }
 
   pageChanged(event: PageEvent) {
     this.page = null;
     this.pageable = new Pageable(event.pageIndex, event.pageSize);
-    this.getGoods();
+    this.loadGoods();
 
   }
 
-  clickOnGoods(goods: GoodsDto) {
+  onRowClicked(goods: GoodsDto) {
     this.goodsSelected.emit(goods);
   }
 
-  private getGoods() {
-    this.goodsService.getAllGoods(2, this.pageable).subscribe((page: Page<GoodsDto>) => {
-      this.page = page;
-    });
-
+  private loadGoods() {
+    this.loadingSubject.next(true);
+    this.goodsService.getAllGoods(2, this.pageable).pipe(
+      catchError(() => of([])),
+      finalize(() => this.loadingSubject.next(false))
+    )
+      .subscribe(page => {
+        if (page instanceof Array) {
+          this.errors = page as any[];
+        } else {
+          this.page = page;
+        }
+      });
   }
 
 }
