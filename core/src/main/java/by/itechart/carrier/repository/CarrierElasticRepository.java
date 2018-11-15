7a910268660ac1b9a337f6bd45a59780d70ac299
chep-by <chep_by@mail.ru>
2018-11-15 15:11:32 +0300

    Bug #786763 fixed Elasticsearch repositories crush app

--- core/src/main/java/by/itechart/carrier/repository/CarrierElasticRepository.java
+++ core/src/main/java/by/itechart/carrier/repository/CarrierElasticRepository.java
@@ -2,10 +2,10 @@ package by.itechart.carrier.repository;
 
 import by.itechart.carrier.entity.Carrier;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchCrudRepository;
 
 
-import java.awt.print.Pageable;
 
 public interface CarrierElasticRepository extends ElasticsearchCrudRepository<Carrier, String> {
     Page<Carrier> findByName(String name, Pageable pageable);
--- core/src/main/java/by/itechart/common/repository/AddressElasticRepository.java
+++ core/src/main/java/by/itechart/common/repository/AddressElasticRepository.java
@@ -2,11 +2,11 @@ package by.itechart.common.repository;
 
 import by.itechart.common.entity.Address;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchCrudRepository;
-import org.springframework.data.jpa.repository.Query;
 
 
 public interface AddressElasticRepository extends ElasticsearchCrudRepository<Address, Long> {
 
-    Page<Address> findByParams(String searchQuery);
+//    Page<Address> findByParams(String param, Pageable pageable);
 }
