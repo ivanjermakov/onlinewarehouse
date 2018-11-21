e36d3771e814dabad523a0c095e614183e8df972
ivanjermakov <vania6600@gmail.com>
2018-11-21 17:08:12 +0300

    #786421 loading template

--- web-app/online-warehouse/src/app/mail/change-birthday-mail-template/change-birthday-mail-template.component.ts
+++ web-app/online-warehouse/src/app/mail/change-birthday-mail-template/change-birthday-mail-template.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
 import {MailService} from '../service/mail.service';
 import {BirthdayMailTemplateDto} from '../dto/BirthdayMailTemplateDto';
 import {BehaviorSubject, of} from 'rxjs';
@@ -19,7 +19,7 @@ export class ChangeBirthdayMailTemplateComponent implements OnInit {
     this.templateForm = fb.group({
       'text': [''],
       'backgroundColor': [''],
-      'headerImage': ['']
+      'headerImagePath': ['']
     });
   }
 
@@ -30,8 +30,11 @@ export class ChangeBirthdayMailTemplateComponent implements OnInit {
   loadBirthdayTemplate() {
     // TODO: don't forget to change to a variable
     this.mailService.getBirthdayMailTemplate(2).subscribe(data => {
-      console.log(data);
       this.template = data;
+      this.templateForm.controls['text'].setValue(this.template.text);
+      this.templateForm.controls['backgroundColor'].setValue(this.template.backgroundColor);
+      // will not set header image because it is probably makes no sense and throws error
+      // this.templateForm.controls['headerImage'].setValue(this.template.headerImagePath);
     });
   }
 
