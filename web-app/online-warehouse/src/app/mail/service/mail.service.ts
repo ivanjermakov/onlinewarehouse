ea41084bf396332d27f82f1de0788035a7fa8580
ivanjermakov <vania6600@gmail.com>
2018-12-09 00:34:21 +0300

    handle mail template edit without specifying image

--- web-app/online-warehouse/src/app/mail/service/mail.service.ts
+++ web-app/online-warehouse/src/app/mail/service/mail.service.ts
@@ -4,7 +4,7 @@ import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {BirthdayMailTemplateDto} from '../dto/BirthdayMailTemplateDto';
 import {UploadService} from '../../upload/service/upload.service';
-import {AuthenticationService} from "../../auth/_services";
+import {AuthenticationService} from '../../auth/_services';
 
 @Injectable({
   providedIn: 'root'
@@ -28,6 +28,12 @@ export class MailService {
                            response: (dto: BirthdayMailTemplateDto) => void) {
     this.uploadService.upload(image).subscribe((imagePath) => {
       birthdayMailTemplateDto.headerImagePath = imagePath;
+      const path: string = this.baseApi + '/' + this.companyId + '/mail-templates/birthday';
+      this.http.put<BirthdayMailTemplateDto>(path, birthdayMailTemplateDto).subscribe((p) => {
+        response(p);
+      });
+    }, () => {
+      birthdayMailTemplateDto.headerImagePath = null;
       const path: string = this.baseApi + '/' + this.companyId + '/mail-templates/birthday';
       this.http.put<BirthdayMailTemplateDto>(path, birthdayMailTemplateDto).subscribe((p) => {
         response(p);
