86ac414d974c4322f9b497b8633b17e3fc41cdc5
student <alexander.chepikov.by@gmail.com>
2018-12-18 12:02:00 +0000

    user password feature

--- core/src/main/java/by/itechart/writeoffact/service/WriteOffActsPredicates.java
+++ core/src/main/java/by/itechart/writeoffact/service/WriteOffActsPredicates.java
@@ -1,6 +1,7 @@
 package by.itechart.writeoffact.service;
 
 import by.itechart.writeoffact.dto.WriteOffActFilter;
+import by.itechart.writeoffact.entity.QWriteOffAct;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.Predicate;
 
@@ -11,16 +12,16 @@ final class WriteOffActsPredicates {
     static Predicate findByWriteOffActFilter(WriteOffActFilter filter, Long companyId) {
         BooleanBuilder predicate = new BooleanBuilder();
 
-//        if (filter.getWriteOffActType() != null) {
-//            predicate.and(QWriteOffAct.writeOffAct.writeOffActType.eq(filter.getWriteOffActType()));
-//        }
-//        if (filter.getFrom() != null) {
-//            predicate.and((QWriteOffAct.writeOffAct.creation.after(filter.getFrom())).or(QWriteOffAct.writeOffAct.creation.eq(filter.getFrom())));
-//        }
-//        if (filter.getTo() != null) {
-//            predicate.and((QWriteOffAct.writeOffAct.creation.before(filter.getTo())).or(QWriteOffAct.writeOffAct.creation.eq(filter.getTo())));
-//        }
-//        predicate.and(QWriteOffAct.writeOffAct.company.id.eq(companyId));
+        if (filter.getWriteOffActType() != null) {
+            predicate.and(QWriteOffAct.writeOffAct.writeOffActType.eq(filter.getWriteOffActType()));
+        }
+        if (filter.getFrom() != null) {
+            predicate.and((QWriteOffAct.writeOffAct.creation.after(filter.getFrom())).or(QWriteOffAct.writeOffAct.creation.eq(filter.getFrom())));
+        }
+        if (filter.getTo() != null) {
+            predicate.and((QWriteOffAct.writeOffAct.creation.before(filter.getTo())).or(QWriteOffAct.writeOffAct.creation.eq(filter.getTo())));
+        }
+        predicate.and(QWriteOffAct.writeOffAct.company.id.eq(companyId));
 
         return predicate;
     }
@@ -28,15 +29,12 @@ final class WriteOffActsPredicates {
     static Predicate findDamages(LocalDate start, LocalDate end) {
         BooleanBuilder predicate = new BooleanBuilder();
 
-//        predicate.and(QWriteOffAct.writeOffAct.writeOffActType.eq(WriteOffActType.LOSS)
-//        .or(QWriteOffAct.writeOffAct.writeOffActType.eq(WriteOffActType.SHORTFALL)));
-//
-//        if (start != null) {
-//            predicate.and(QWriteOffAct.writeOffAct.creation.after(start).or(QWriteOffAct.writeOffAct.creation.eq(start)));
-//        }
-//        if (end != null) {
-//            predicate.and(QWriteOffAct.writeOffAct.creation.after(end).or(QWriteOffAct.writeOffAct.creation.eq(end)));
-//        }
+        if (start != null) {
+            predicate.and(QWriteOffAct.writeOffAct.creation.after(start).or(QWriteOffAct.writeOffAct.creation.eq(start)));
+        }
+        if (end != null) {
+            predicate.and(QWriteOffAct.writeOffAct.creation.after(end).or(QWriteOffAct.writeOffAct.creation.eq(end)));
+        }
 
         return predicate;
     }
